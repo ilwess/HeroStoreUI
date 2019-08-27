78ab5b8c7e60a810c9b119bc5b5530b4315658b6
ilwess <pasha.vrublevskiy20@list.ru>
2019-08-27 12:28:39 +0300

    add main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
